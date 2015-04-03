ed6b68297cf91abbf40ab954593d976a0bc9314a
Christopher Davis <cdavis9999@gmail.com>
2015-04-03 15:53:19 -0400

    Set up the skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# PmgElasticsearchBundle
+
+This is an extremely simple bundle to integrate Elasticsearch into Symfony. It
+only provides some configuration and the elasticsearch client.
---
+++ src/PmgElasticsearchBundle.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of PMG Elasticsearch Bundle
+ *
+ * @package     PMG\ElasticsearchBundle
+ * @license     http://opensource.org/licenses/mit mit
+ * @copyright   PMG <https://www.pmg.co>
+ */
+
+namespace PMG\ElasticsearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Our bundle entry point.
+ *
+ * @since   0.1
+ */
+final class PmgElasticsearchBundle extends Bundle
+{
+
+}
