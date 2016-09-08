f3ece9723942707e29239579787a3326f257c04e
Christopher Davis <cdavis9999@gmail.com>
2016-09-07 20:25:51 -0500

    Update the Bundle to use Elasticsearch PHP 2.X
    Some configuration changes to support the way the 2.X works. Also remove
    our built in factory in favor of using the built in `ClientBuilder`.

--- src/DependencyInjection/PmgElasticsearchExtension.php
+++ src/DependencyInjection/PmgElasticsearchExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
-use PMG\ElasticsearchBundle\ElasticsearchFactory;
+use Elasticsearch\ClientBuilder;
 
 /**
  * Our DI container extension.
@@ -23,17 +23,6 @@ use PMG\ElasticsearchBundle\ElasticsearchFactory;
  */
 final class PmgElasticsearchExtension extends ConfigurableExtension
 {
-    private $optionsMap = [
-        'connection_class'              => 'connectionClass',
-        'connection_factory_class'      => 'connectionFactoryClass',
-        'connectoin_pool_class'         => 'connectionPoolClass',
-        'selector_class'                => 'selectorClass',
-        'serializer_class'              => 'serializerClass',
-        'sniff_on_start'                => 'sniffOnStart',
-        'enable_logging'                => 'logging',
-        'hosts'                         => 'hosts',
-    ];
-
     /**
      * {@inheritdoc}
      */
@@ -52,32 +41,42 @@ final class PmgElasticsearchExtension extends ConfigurableExtension
     private function addClient(ContainerBuilder $container, $name, array $config)
     {
         $factory = $container->setDefinition(
-            "pmg_elasticsearch.{$name}.client_factory",
-            new Definition(ElasticsearchFactory::class, [
-                $this->createClientArguments($config),
-                new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE),
-            ])
+            "pmg_elasticsearch.{$name}.client_builder",
+            new Definition(ClientBuilder::class)
         );
-        $factory->addTag('monolog.logger', [
-            'channel'   => 'pmg_elasticsearch'
-        ]);
+        $factory->addMethodCall('setSniffOnStart', [!empty($config['sniff_on_start'])]);
+        $factory->addMethodCall('setHosts', [$config['hosts']]);
+
+        if ($config['enable_logging']) {
+            $factory->addTag('monolog.logger', [
+                'channel'   => 'pmg_elasticsearch'
+            ]);
+            $factory->addMethodCall('setLogger', [
+                new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE),
+            ]);
+            $factory->addMethodCall('setTracer', [
+                new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE),
+            ]);
+        }
+
+        if (!empty($config['connection_factory'])) {
+            $factory->addMethodCall('setConnectionFactory', [new Reference($config['connection_factory'])]);
+        }
+
+        if (!empty($config['connection_pool'])) {
+            $factory->addMethodCall('setConnectionPool', [new Reference($config['connection_pool'])]);
+        }
+
+        if (!empty($config['serializer'])) {
+            $factory->addMethodCall('setSerializer', [new Reference($config['serializer'])]);
+        } elseif (!empty($config['serializer_class'])) {
+            $factory->addMethodCall('setSerializer', [$config['serializer_class']]);
+        }
 
         $client = $container->setDefinition(
             "pmg_elasticsearch.{$name}.client",
             new Definition(\Elasticsearch\Client::class)
         );
-        $client->setFactory([new Reference("pmg_elasticsearch.{$name}.client_factory"), 'create']);
-    }
-
-    private function createClientArguments(array $config)
-    {
-        $arguments = [];
-        foreach ($this->optionsMap as $bundle => $es) {
-            if (!empty($config[$bundle])) {
-                $arguments[$es] = $config[$bundle];
-            }
-        }
-
-        return $arguments;
+        $client->setFactory([new Reference("pmg_elasticsearch.{$name}.client_builder"), 'build']);
     }
 }
